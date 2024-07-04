62642c3ff94cab24b648ec6d6c2e72e59dce0c22
NainiKhan <hasnainashfaq4411@gmail.com>
2024-07-04 12:43:01 +0100

    python

--- Chap_4_Lists_&_Tuples/Practise_Test_List_Tuples.py
+++ Chap_4_Lists_&_Tuples/Practise_Test_List_Tuples.py
@@ -51,7 +51,8 @@
 # Q4: Write a program to sum a list with 4 numbers.
 
 # a = [2,2,2,2]
-# print([a[0]+a[1]+a[2]+a[3]])
+# print([a[0]+a[1]+a[2]+a[3]]) # We can also use this but there is also another way to do this
+# print(sum(a)) # This Sum funcion can add all the integers of the List
 
 # Q5: Write a program to count the number of zeros in the following tuple: Tuple is a = (7, 0, 8, 0, 0, 9)
 
