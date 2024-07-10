435b22eff0372dc1898bf693c719dd6d47294b66
NainiKhan <hasnainashfaq4411@gmail.com>
2024-07-10 11:35:18 +0100

    python

--- Chap_5_Dictionary_and_Sets/distionaries.py
+++ Chap_5_Dictionary_and_Sets/distionaries.py
@@ -18,3 +18,37 @@ print(marks)
 # We will simply call the Key name to get that value
 
 print(marks['Dart'])
+
+#_________METHODS IN DICTIONARY_________________
+#1: .item() this method give us the result in the form of tuples
+print(marks.items())
+
+#2: keys() this method give us the only keys of dictionary
+print(marks.keys())
+
+#3 values() This method will just print the values of Marks Dictionary
+print(marks.values())
+
+#4 Updating the Dictionary so we will enter the Key and also we will enter the new value
+marks.update({"Dart":99}) #Similarly Like distionary we will call the Key in Curly Brackets and assign the value in it 
+print(marks) # Now the original dictionary will be updated the reason is Dictionary is Mutable
+# Jo Key dictionary ka andr hogi aus ki value update hogi or jo nhi hogi aus ki new key or value bna dy ga
+#5 Adding new Key and Value so to add new key and value in dictionary we will use the same method if Ruby is not in dictionary so it will ad it
+marks.update({"Ruby":7829})
+print(marks)  # So ruby is now added as a key and 7829 is a value of Rubys
+
+#6 Get the  value of specific key using this get() method
+print(marks.get("Dart")) # This wil specifically retured the value of Dart key
+#print(marks['Dart']) and marks.get("Dart") # Both will works same but  the point is if the key not exists in dictionary
+# In that case ['Dart'] this will return error but the .get("Dart") this will return None.
+
+#7 clear Method this will remove all Keys and values from the Dictionary
+
+# marks.clear()
+# print(marks)
+
+# remove singel key and its value form Dictionary
+marks.pop('Dart')#
+print(marks)
+
+
---
+++ Chap_5_Dictionary_and_Sets/sets.py
@@ -0,0 +1,8 @@
+s = {12,12,4,1} # this is the way to Write Set
+s  =() # This is a empty set but in order to add values in it you need to use  {} brackets
+
+v ={12,5,5,5,5,}# This set contains repeated values (5 written 4 time) but Set will save 5 only one time
+
+print(v) # THis will only show 12,5 (5 will be printed only one time so in set values can't be repeated)
+
+
