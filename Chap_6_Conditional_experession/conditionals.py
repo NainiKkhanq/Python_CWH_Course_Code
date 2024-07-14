e242745571ac321a90946d6a25b86cd56899699c
NainiKhan <hasnainashfaq4411@gmail.com>
2024-07-14 16:11:28 +0100

    py

--- Chap_6_Conditional_experession/conditionals.py
+++ Chap_6_Conditional_experession/conditionals.py
@@ -1,10 +1,14 @@
 age = int(input("Enter your age: "))
 
         # IF ELSE
-
+    # If ElseIf Else ladder
 if (age>=18):
     print("Welcome You are allowed to Play Games")
     print("Best of Luck")
+elif(age<0):
+     print("You are Entering Wrong Age")
+elif(age==0):
+     print("You Entered 0 Which is invalid Age.")
 else:
      print("Oh I am sorry :( You are not allowed to Play Games")
      print("Better Luck Next Time")
