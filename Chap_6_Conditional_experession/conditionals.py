1c650796bd43d4f9bee85025f0097456dbb91e91
NainiKhan <hasnainashfaq4411@gmail.com>
2024-07-14 09:30:11 +0100

    py

---
+++ Chap_6_Conditional_experession/conditionals.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+
+        # IF ELSE
+
+if (age>=18):
+    print("Welcome You are allowed to Play Games")
+    print("Best of Luck")
+else:
+     print("Oh I am sorry :( You are not allowed to Play Games")
+     print("Better Luck Next Time")
+
+        # EL IF (else if)
