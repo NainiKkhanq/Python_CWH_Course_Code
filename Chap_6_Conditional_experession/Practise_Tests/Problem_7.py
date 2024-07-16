49a03595363f7951b2625292375a394b7ffaf00d
NainiKhan <hasnainashfaq4411@gmail.com>
2024-07-16 16:33:32 +0100

    python

--- Chap_6_Conditional_experession/Practise_Tests/Problem_7.py
+++ Chap_6_Conditional_experession/Practise_Tests/Problem_7.py
@@ -2,7 +2,8 @@
 
 post = input("Type Your Post: ")
 a = "Harry"
-if((a in post)):
+# .lower() will convert the text to lower case and than check the condition
+if(a.lower() in post.lower()):
     print("Harry Found")
 else:
     print("Harry Not Found")
